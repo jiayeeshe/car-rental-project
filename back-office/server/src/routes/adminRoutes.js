f8716b6edab90fa3ba87e2938e4acffc95489ed7
JIA YEE SHE <shejiayee@gmail.com>
2024-09-03 22:25:12 +0800

    deleted redundant server folder from back-office

--- back-office/server/src/routes/adminRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminControllers');
-
-router.post("/register", adminController.register);
-// router.post("/login", adminController.login)
-module.exports= router;
